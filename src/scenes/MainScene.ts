df6dd9f14377d5ccb195286d57e62624aab04190
kKondratek <k.kondratek@o2.pl>
2020-07-24 20:35:29 +0200

    change main character and animations

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -15,45 +15,59 @@ export default class MainScene extends Phaser.Scene {
         this.load.image('ground', 'assets/ground.png');
         this.load.image('grass', 'assets/grass.png');
         this.load.image('platform', 'assets/platform.png')
-        this.load.spritesheet('dude', 'assets/dude.png',
-            {frameWidth: 32, frameHeight: 48});
+        this.load.spritesheet('player-idle', 'assets/player-idle.png',
+            {frameWidth: 184, frameHeight: 137});
+        this.load.spritesheet('player-run-right', 'assets/player-run-right.png',
+            {frameWidth: 184, frameHeight: 137});
+        this.load.spritesheet('player-run-left', 'assets/player-run-left.png',
+            {frameWidth: 184, frameHeight: 137});
+        this.load.spritesheet('player-crouch', 'assets/player-crouch.png',
+            {frameWidth: 184, frameHeight: 137});
     }
 
     create() {
         this.add.image(400, 200, 'background');
 
         this.platform = this.physics.add.staticGroup();
-        const group = this.platform.create(400, 568, 'platform');
+        const group = this.platform.create(400, 578, 'platform');
 
-        // group.setScale(1)
-        //     .refreshBody();
-
-        this.player = this.physics.add.sprite(100,500, 'dude');
+        this.player = this.physics.add.sprite(100,400, 'player-idle');
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
 
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', {
-                start: 5, end: 8
+            frames: this.anims.generateFrameNumbers('player-run-right', {
+                start: 0, end: 7
             }),
-            frameRate: 10,
+            frameRate: 12,
             repeat: -1
         });
 
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', {
-                start: 0, end: 3
+            frames: this.anims.generateFrameNumbers('player-run-left', {
+                start: 0, end: 7
             }),
-            frameRate: 10,
+            frameRate: 12,
             repeat: -1
         });
 
         this.anims.create({
             key: 'idle',
-            frames: [{key: 'dude', frame: 4}],
-            frameRate: 20
+            frames: this.anims.generateFrameNumbers('player-idle', {
+                start: 0, end: 5
+            }),
+            frameRate: 6
+        });
+
+        this.anims.create({
+            key: 'crouch',
+            frames: this.anims.generateFrameNumbers('player-crouch', {
+                start: 0, end: 3
+            }),
+            frameRate: 6,
+            repeat: -1
         });
 
         this.physics.add.collider(this.player, this.platform);
@@ -70,11 +84,14 @@ export default class MainScene extends Phaser.Scene {
         }
 
         if (this.cursors.right?.isDown) {
-            this.player?.setVelocityX(60);
+            this.player?.setVelocityX(150);
             this.player?.anims.play('right', true);
         } else if (this.cursors.left?.isDown) {
-            this.player?.setVelocityX(-60);
+            this.player?.setVelocityX(-150);
             this.player?.anims.play('left', true);
+        } else if (this.cursors.down?.isDown) {
+            this.player?.setVelocityX(0);
+            this.player?.anims.play('crouch', true);
         } else {
             this.player?.setVelocityX(0);
             this.player?.anims.play('idle', true);
